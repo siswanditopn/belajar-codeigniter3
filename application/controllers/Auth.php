4d6653c5c62a63c1ad214c9b8e7685b4ed67da29
Siswandito Putro Nugroho <siswanditopn@gmail.com>
2022-08-17 05:35:16 +0700

    Beri title

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -5,6 +5,7 @@ class Auth extends CI_Controller
 {
     public function index()
     {
+        $data['title'] = "Login";
         $this->load->view('template/auth_header');
         $this->load->view('auth/login');
         $this->load->view('template/auth_footer');
@@ -12,7 +13,8 @@ class Auth extends CI_Controller
 
     public function registration()
     {
-        $this->load->view('template/auth_header');
+        $data['title'] = "Registration";
+        $this->load->view('template/auth_header', $data);
         $this->load->view('auth/registration');
         $this->load->view('template/auth_footer');
     }
--- application/views/template/auth_header.php
+++ application/views/template/auth_header.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Belajar CodeIgniter 3</title>
+    <title>Belajar CodeIgniter 3 | <?= $title; ?></title>
 
     <!-- Custom fonts for this template-->
     <link href="<?= base_url('assets/'); ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
