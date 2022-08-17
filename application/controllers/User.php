d73d7a2902b48d7fefb5cb5d838c85e4a979781c
Siswandito Putro Nugroho <siswanditopn@gmail.com>
2022-08-17 22:00:34 +0700

    User Access

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -3,6 +3,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User extends CI_Controller
 {
+  public function __construct()
+  {
+    parent::__construct();
+
+    if (!$this->session->userdata('email')) {
+      redirect('auth');
+    }
+  }
+  
   public function index()
   {
     $data['title'] = 'My Profile';
