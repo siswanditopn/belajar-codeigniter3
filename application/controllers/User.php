8ca6757698370ea30b3c3d9f2e17680b3e071cfc
Siswandito Putro Nugroho <siswanditopn@gmail.com>
2022-08-17 13:48:50 +0700

    Templating

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -8,6 +8,10 @@ class User extends CI_Controller
     $data['title'] = 'My Profile';
     $data['user']  = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
     
+    $this->load->view('template/header', $data);
+    $this->load->view('template/sidebar', $data);
+    $this->load->view('template/topbar', $data);
     $this->load->view('user/index', $data);
+    $this->load->view('template/footer');
   }
 }
